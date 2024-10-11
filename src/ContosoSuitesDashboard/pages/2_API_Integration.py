eac7c7e65fbfe85e24070c0d320c00eb69343540
franciscobertao1995 <60822464+franciscobertao1995@users.noreply.github.com>
2024-10-11 08:22:45 +0000

    APIupdate

--- src/ContosoSuitesDashboard/pages/2_API_Integration.py
+++ src/ContosoSuitesDashboard/pages/2_API_Integration.py
@@ -6,14 +6,16 @@ st.set_page_config(layout="wide")
 @st.cache_data
 def get_hotels():
     """Return a list of hotels from the API."""
-    api_endpoint = st.secrets["api"]["endpoint"]
+    #api_endpoint = st.secrets["api"]["endpoint"]
+    api_endpoint = "oiuqhyfs52p4q-api.azurewebsites.net"
     response = requests.get(f"{api_endpoint}/Hotels", timeout=10)
     return response
 
 @st.cache_data
 def get_hotel_bookings(hotel_id):
     """Return a list of bookings for the specified hotel."""
-    api_endpoint = st.secrets["api"]["endpoint"]
+    #api_endpoint = st.secrets["api"]["endpoint"]
+    api_endpoint = "oiuqhyfs52p4q-api.azurewebsites.net"
     response = requests.get(f"{api_endpoint}/Hotels/{hotel_id}/Bookings", timeout=10)
     return response
 
